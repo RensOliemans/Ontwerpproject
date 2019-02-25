69c70af8a25ac2a3c1878f65010a4b68fc93e96c
Rens Oliemans <rens.oliemans@gmail.com>
2019-02-25 15:08:54 +0100

    :sparkles: Add dotenv things

--- wappie/wappie/settings.py
+++ wappie/wappie/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'wappie.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("POSTGRESQL_DATABASENAME"),
+        'USER': os.getenv("POSTGRESQL_USERNAME"),
+        'PASSWORD': os.getenv("POSTGRESQL_PASSWORD"),
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
@@ -105,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Amsterdam'
 
 USE_I18N = True
 
